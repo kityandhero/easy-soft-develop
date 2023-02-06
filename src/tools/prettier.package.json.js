d83fb0262cdf9964d07fb12e57b7a119f7bf2cba
卢志涛 <kityandhero@126.com>
2023-02-06 15:42:50 +0800

    fix: update

--- src/tools/prettier.package.json.js
+++ src/tools/prettier.package.json.js
@@ -1,10 +1,17 @@
+const { promptInfo } = require('./meta');
 const { exec } = require('./shell');
 
 function prettierAllPackageJson() {
+  console.log('');
+  promptInfo('will format all package.json');
+
   exec('npx prettier --write ./**/package.json');
 }
 
 function prettierCurrentPackageJson() {
+  console.log('');
+  promptInfo('will format current package.json');
+
   exec('npx prettier --write ./package.json');
 }
 
