6aa10941499ec856e45f6736a3671ce0e05208db
卢志涛 <kityandhero@126.com>
2023-02-06 15:36:11 +0800

    fix: update

--- src/tools/config.environment.js
+++ src/tools/config.environment.js
@@ -58,54 +58,39 @@ function createPackageFile(fileWithContentCollection) {
 function adjustMainPackageJson({ scripts }) {
   const mainProjectPath = resolve(`./package.json`);
 
-  fsExtra
-    .readJson(mainProjectPath)
-    .then((packageJson) => {
-      const globalScript = {
-        prepare: 'husky install',
-        postinstall: 'npm run z:initial:environment',
-        commitlint: 'npx commitlint --edit',
-        precz: 'npm run z:commit:refresh && git stage -A',
-        cz: 'cz',
-        postcz: 'git push',
-        'z:initial:environment': 'node ./develop/assists/initial.environment.js',
-        'z:prettier:format:all': 'npx prettier --write .',
-        'z:prettier:format:change': 'npx prettier --cache --write .',
-        'z:prettier:package.json:all': 'npx prettier --write ./**/package.json',
-        'z:prettier:package.json:current': 'npx prettier --write ./package.json',
-        'z:change-nrm-local': 'nrm use local',
-        'z:change-nrm-npm': 'nrm use npm',
-        'z:install': 'echo please set install cmd with here',
-        'z:sleep': 'npx easy-soft-develop sleep --second 2 --showInfo false',
-        'z:check-all-package-version': 'npx easy-soft-develop check-all-package-version',
-        'z:update-all-package-version': 'npx easy-soft-develop update-all-package-version',
-        'postz:update-all-package-version': 'npm run z:install',
-        'z:update-special-package-version': 'node ./develop/assists/package.update.special.version.js',
-        'postz:update-special-package-version': 'npm run z:install',
-        'z:clean': 'node ./develop/assists/clean.js',
-        'z:commit:refresh': 'npx easy-soft-develop commit-refresh',
-        'z:create:assist:scripts': 'npx easy-soft-develop create-assist-scripts',
-      };
+  const packageJson = fsExtra.readJsonSync(mainProjectPath);
 
-      packageJson.scripts = assignObject(globalScript, packageJson.scripts || {}, scripts);
+  const globalScript = {
+    prepare: 'husky install',
+    postinstall: 'npm run z:initial:environment',
+    commitlint: 'npx commitlint --edit',
+    precz: 'npm run z:commit:refresh && git stage -A',
+    cz: 'cz',
+    postcz: 'git push',
+    'z:initial:environment': 'node ./develop/assists/config.environment.js',
+    'z:prettier:format:all': 'npx prettier --write .',
+    'z:prettier:format:change': 'npx prettier --cache --write .',
+    'z:prettier:package.json:all': 'npx prettier --write ./**/package.json',
+    'z:prettier:package.json:current': 'npx prettier --write ./package.json',
+    'z:change-nrm-local': 'nrm use local',
+    'z:change-nrm-npm': 'nrm use npm',
+    'z:install': 'echo please set install cmd with here',
+    'z:sleep': 'npx easy-soft-develop sleep --second 2 --showInfo false',
+    'z:check-all-package-version': 'npx easy-soft-develop check-all-package-version',
+    'z:update-all-package-version': 'npx easy-soft-develop update-all-package-version',
+    'postz:update-all-package-version': 'npm run z:install',
+    'z:update-special-package-version': 'node ./develop/assists/package.update.special.version.js',
+    'postz:update-special-package-version': 'npm run z:install',
+    'z:clean': 'node ./develop/assists/clean.js',
+    'z:commit:refresh': 'npx easy-soft-develop commit-refresh',
+    'z:create:assist:scripts': 'npx easy-soft-develop create-assist-scripts',
+  };
 
-      fsExtra
-        .writeJson(mainProjectPath, packageJson)
-        .then(() => {
-          promptSuccess('adjust main package.json success');
+  packageJson.scripts = assignObject(globalScript, packageJson.scripts || {}, scripts);
 
-          return;
-        })
-        .catch((err) => {
-          console.error(err);
-          return;
-        });
+  fsExtra.writeJsonSync(mainProjectPath, packageJson);
 
-      return;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  promptSuccess('adjust main package.json success');
 }
 
 function adjustChildrenPackageJson({ scripts }) {
@@ -114,31 +99,13 @@ function adjustChildrenPackageJson({ scripts }) {
 
     const childPackageJsonPath = `${itemPath}/package.json`;
 
-    fsExtra
-      .readJson(childPackageJsonPath)
-      .then((packageJson) => {
-        packageJson.scripts = assignObject(packageJson.scripts || {}, scripts);
+    const packageJson = fsExtra.readJsonSync(childPackageJsonPath);
 
-        fsExtra
-          .writeJson(childPackageJsonPath, packageJson)
-          .then(() => {
-            promptSuccess('adjust child package.json success');
+    packageJson.scripts = assignObject(packageJson.scripts || {}, scripts);
 
-            return;
-          })
-          .catch((e) => {
-            console.error(e);
+    fsExtra.writeJsonSync(childPackageJsonPath, packageJson);
 
-            return;
-          });
-
-        return;
-      })
-      .catch((res) => {
-        console.error(res);
-
-        return;
-      });
+    promptSuccess('adjust child package.json success');
   });
 }
 
