53dc31f791030fe961f5ba47ee0455ff114bb9f4
卢志涛 <kityandhero@126.com>
2023-02-07 17:15:46 +0800

    fix: update

--- test/assignObject.test.js
+++ test/assignObject.test.js
@@ -3,3 +3,7 @@ const { assignObject, promptInfo } = require('../src/tools/meta');
 const s = assignObject({}, { a: 1 }, { b: 2 }, { c: 3 });
 
 promptInfo(JSON.stringify(s));
+
+const s1 = assignObject({}, { a: 1 });
+
+promptInfo(JSON.stringify(s1));
