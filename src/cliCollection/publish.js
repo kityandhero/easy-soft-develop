d6a4cde4d662bf89d26f33d0dc273eeb2b5699ce
卢志涛 <kityandhero@126.com>
2023-02-20 10:14:36 +0800

    fix: update

--- src/cliCollection/publish.js
+++ src/cliCollection/publish.js
@@ -1,9 +1,31 @@
-const { exit, exec } = require('../tools/meta');
+const {
+  exit,
+  exec,
+  promptInfo,
+  promptSuccess,
+  promptError,
+} = require('../tools/meta');
 
 exports.run = function () {
-  exec('npm run npm run z:change:npm:registry:npm');
-  exec(`npm run z:publish:npm-all`);
-  exec('npm run npm run z:change:npm:registry:local');
+  promptInfo(
+    'change npm registry to npm by script "z:change:npm:registry:npm"',
+  );
+
+  exec('npm run z:change:npm:registry:npm');
+
+  try {
+    exec(`npm run z:publish:npm-all`);
+  } catch (error) {
+    promptError(error);
+  }
+
+  promptInfo(
+    'change npm registry to local by script "z:change:npm:registry:local"',
+  );
+
+  exec('npm run z:change:npm:registry:local');
+
+  promptSuccess('publish complete');
 
   exit();
 };
