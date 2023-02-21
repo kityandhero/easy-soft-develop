2e1e449f11735e545aef38fe7f7dd613d8655d55
卢志涛 <kityandhero@126.com>
2023-02-21 11:23:22 +0800

    fix: update

--- src/tools/clean.js
+++ src/tools/clean.js
@@ -8,16 +8,48 @@ const {
 const { loopPackage } = require('./package.tools');
 
 function adjustMainPackageJson(command) {
+  promptInfo(`clean main: ${command}`);
+
   exec(command);
 }
 
+function tryCleanChildrenPackage(cmd, tryTimes) {
+  if (tryTimes > 5) {
+    promptInfo(`clean package fail, ignore`);
+
+    return;
+  }
+
+  if (tryTimes > 1) {
+    promptInfo(`retry time ${tryTimes}`);
+  }
+
+  try {
+    exec(cmd);
+  } catch (error) {
+    tryTimes = tryTimes + 1;
+
+    tryCleanChildrenPackage(cmd, tryTimes);
+  }
+}
+
 function adjustChildrenPackageJson(command) {
-  loopPackage(({ name }) => {
-    exec(`cd ./packages/${name} && ${command}`);
+  loopPackage(({ name, relativePath }) => {
+    const cmd = `cd ${relativePath} && ${command}`;
+
+    promptInfo(`clean package ${name}: ${cmd}`);
+
+    let tryTimes = 1;
+
+    tryCleanChildrenPackage(cmd, tryTimes);
   });
 }
 
 function clean(preCmd, ...targets) {
+  promptInfo(
+    'clean use rimraf, ensure rimraf is installed, install it use "npm install -g rimraf"',
+  );
+
   try {
     const list = [...targets];
 
@@ -26,7 +58,7 @@ function clean(preCmd, ...targets) {
     const command = list
       .map((o) => {
         if (o) {
-          return `npx rimraf ./${o}`;
+          return `rimraf ./${o}`;
         }
         return '';
       })
@@ -36,6 +68,8 @@ function clean(preCmd, ...targets) {
     promptEmptyLine();
 
     if (preCmd) {
+      promptInfo(`clean by command: ${preCmd}`);
+
       exec(preCmd);
     }
 
